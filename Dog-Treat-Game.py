1c78abe83ef2944b954f991abaa62c0263585695
Valeho <van.lehoang111@gmail.com>
2024-11-11 19:08:29 +0000

    fix indentation

--- Dog-Treat-Game.py
+++ Dog-Treat-Game.py
@@ -72,9 +72,9 @@ treat = pygame.transform.scale(treat, (treat_size[0], treat_size[1]))
 # action 0 = sitting
 # action 1 = walking right
 # action 2 = walking left
-dog = [ [dog_sitting1, dog_sitting2], 
-	[dog_walking_right1, dog_walking_right2], 
-	[dog_walking_left1, dog_walking_left2] ]
+dog = [[dog_sitting1, dog_sitting2], 
+       [dog_walking_right1, dog_walking_right2], 
+       [dog_walking_left1, dog_walking_left2]]
 
 health = [heart_full, heart_empty]
 							
@@ -104,14 +104,16 @@ opacity = 255
 
 
 treat1_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
-																		(window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), 
-																	 	window_y - game_border_y + 10)
+                                          (window_x - game_x) // 2 + game_x - dog_size // 2,
+                                          treat_size[0]), 
+                         window_y - game_border_y + 10)
 treat2_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
-																		(window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), 
-																	 	window_y - game_border_y +10)
+                                          (window_x - game_x) // 2 + game_x - dog_size // 2,
+                                          treat_size[0]),
+                         window_y - game_border_y +10)
 heart_treat_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
-																		(window_x - game_x) // 2 + game_x - dog_size // 2, heart_size), 
-																	 	window_y - game_border_y +10)
+                                               (window_x - game_x) // 2 + game_x - dog_size // 2, heart_size),
+                              window_y - game_border_y +10)
 
 
 running = True
@@ -132,7 +134,7 @@ def show_score(color, font, size):
     # text surface object
     score_rect = score_surface.get_rect()
     score_rect = ((window_x - game_border_x) // 2, 
-											 window_y - game_border_y - size - 10)
+                  window_y - game_border_y - size - 10)
     # displaying text
     game_window.blit(score_surface, score_rect)
 	
@@ -151,18 +153,18 @@ def get_point(color, font, size, opacity, point_box, point):
     
     # displaying text
     if opacity >= 30:
-    	opacity -= 30
-    	size += 7
-    	point_surface.set_alpha(opacity)
-    	point_box.y -= 11
-    	font_rect.center = point_box.center
-    	game_window.blit(point_surface, font_rect)
+      opacity -= 30
+      size += 7
+      point_surface.set_alpha(opacity)
+      point_box.y -= 11
+      font_rect.center = point_box.center
+      game_window.blit(point_surface, font_rect)
     else:
-    	size += 7
-    	point_surface.set_alpha(0)
-    	point_box.y -= 11
-    	font_rect.center = point_box.center
-    	game_window.blit(point_surface, font_rect)
+      size += 7
+      point_surface.set_alpha(0)
+      point_box.y -= 11
+      font_rect.center = point_box.center
+      game_window.blit(point_surface, font_rect)
     	
     return opacity, size, point_box
 
@@ -174,8 +176,7 @@ def health_check(max_health, damage):
 	full_heart = max_health
 	for i in range(max_health):
 		heart_rect = pygame.Rect((window_x - game_border_x) // 2 + game_border_x - heart_size * (i + 1), 
-																			  		window_y - game_border_y - heart_size + 10, 
-																			  		heart_size, heart_size)
+                             window_y - game_border_y - heart_size + 10, heart_size, heart_size)
 		if full_heart - damage > 0:
 			game_window.blit(health[0], heart_rect)
 			full_heart -= 1
@@ -207,14 +208,14 @@ def branding(color, font, size):
 # display dog animation based on action
 def dog_animation(action, frame, update_time):
   	
-  	current_time = time.time()
-  	if current_time - update_time >= frame_rate:
-  		frame = (frame + 1) % len(dog[action])
-  		update_time = current_time
+  current_time = time.time()
+  if current_time - update_time >= frame_rate:
+      frame = (frame + 1) % len(dog[action])
+      update_time = current_time
   	
-  	game_window.blit(dog[action][frame], dog_rect)
+  game_window.blit(dog[action][frame], dog_rect)
   	
-  	return frame, update_time
+  return frame, update_time
  
  
  
@@ -266,9 +267,15 @@ def game_reset():
     
     dog_rect.midbottom = (window_x // 2, window_y - 10)
     
-    treat1_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, (window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), window_y - game_border_y + 10)
-    treat2_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, (window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), window_y - game_border_y + 10)
-    heart_treat_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, (window_x - game_x) // 2 + game_x - dog_size // 2, heart_size), window_y - game_border_y + 10)
+    treat1_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
+                                              (window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), 
+                             window_y - game_border_y + 10)
+    treat2_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
+                                              (window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), 
+                             window_y - game_border_y + 10)
+    heart_treat_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
+                                                   (window_x - game_x) // 2 + game_x - dog_size // 2, heart_size), 
+                                  window_y - game_border_y + 10)
     
 
   
@@ -326,171 +333,166 @@ def game_over(game_box):
                   
         
 while True:
- 	
- 	# fill backgound and draw game window
- 	game_window.fill(beige)
- 	
- 	game_border = pygame.Rect(0, 0, game_border_x, game_border_y)
- 	game_border.midbottom = (window_x // 2, window_y)
- 	pygame.draw.rect(game_window, mud, game_border)
- 	
- 	game_rect = pygame.Rect(0, 0, game_x, game_y)
- 	game_rect.center = game_border.center
- 	pygame.draw.rect(game_window, beige, game_rect)
- 	
- 	
- 	# display score
- 	show_score(mud, 'Roboto', 40)
- 	
- 	# display branding
- 	branding(mud, 'Roboto', 50)
- 	
- 	for event in pygame.event.get():
+  # fill backgound and draw game window
+  game_window.fill(beige)
+  game_border = pygame.Rect(0, 0, game_border_x, game_border_y)
+  game_border.midbottom = (window_x // 2, window_y)
+  pygame.draw.rect(game_window, mud, game_border)
+  
+  game_rect = pygame.Rect(0, 0, game_x, game_y)
+  game_rect.center = game_border.center
+  pygame.draw.rect(game_window, beige, game_rect)
+  
+  # display score
+  show_score(mud, 'Roboto', 40)
+  
+  # display branding
+  branding(mud, 'Roboto', 50)
+  
+  for event in pygame.event.get():
  		if event.type == pygame.QUIT:
  				running = False
- 		
- 	# check key state for continuous movement
- 	keys = pygame.key.get_pressed()		
- 	
- 	if keys[pygame.K_RIGHT]:
- 		direction = 'RIGHT'
- 		action = 1
- 		dog_rect.x += 10
- 	elif keys[pygame.K_LEFT]:
- 		direction = 'LEFT'
- 		action = 2
- 		dog_rect.x -= 10	
- 	else: 
- 		direction = 'IDLE'
- 		action = 0
- 		
- 	# limit dog movement inside game frame
- 	if dog_rect.x <= game_rect.x :
- 		direction = 'IDLE'
- 		action = 0
- 		dog_rect.x += 10
- 	elif dog_rect.x + dog_size >= game_rect.x + game_x:
- 		direction = 'IDLE'
- 		action = 0
- 		dog_rect.x -= 10
- 	 
- 	# dog animation
- 	frame, update_time = dog_animation(action, frame, update_time)
+      
+  # check key state for continuous movement
+  keys = pygame.key.get_pressed()
+  
+  if keys[pygame.K_RIGHT]:
+    direction = 'RIGHT'
+    action = 1
+    dog_rect.x += 10
+  elif keys[pygame.K_LEFT]:
+    direction = 'LEFT'
+    action = 2
+    dog_rect.x -= 10
+  else:
+    direction = 'IDLE'
+    action = 0
+    
+  # limit dog movement inside game frame
+  if dog_rect.x <= game_rect.x:
+    direction = 'IDLE'
+    action = 0
+    dog_rect.x += 10
+  elif dog_rect.x + dog_size >= game_rect.x + game_x:
+    direction = 'IDLE'
+    action = 0
+    dog_rect.x -= 10
+    
+  # dog animation
+  frame, update_time = dog_animation(action, frame, update_time)
  	
  	# there is a chance to spawn heart treat
- 	if not heart_treat and random.randrange(1, 77, 1) == 7:
+  if not heart_treat and random.randrange(1, 77, 1) == 7:
  			heart_treat = True
- 	
- 	# heart treat drop
- 	if heart_treat:		
- 		hit_bottom_heart, heart_treat_rect = treat_drop(heart_full, heart_treat_rect, game_rect.top, game_rect.bottom, heart_drop_speed, hit_bottom_heart)
+    
+  # heart treat drop
+  if heart_treat:
+    hit_bottom_heart, heart_treat_rect = treat_drop(heart_full, heart_treat_rect, game_rect.top, game_rect.bottom, heart_drop_speed, hit_bottom_heart)
  	
  	# drop heart treat doesn't cause damage
- 	if hit_bottom_heart:
- 		heart_treat = False
- 		heart_treat_drop_speed = random.randrange(7, 11, 1)
- 		heart_treat_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
-																		(window_x - game_x) // 2 + game_x - dog_size // 2, heart_size), 
-																	 	window_y - game_border_y + 10)
+  if hit_bottom_heart:
+    heart_treat = False
+    heart_treat_drop_speed = random.randrange(7, 11, 1)
+    heart_treat_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
+                                                   (window_x - game_x) // 2 + game_x - dog_size // 2, heart_size), 
+                                  window_y - game_border_y + 10)
 
  	# getting health for heart treat
- 	overlap_heart = dog_rect.clip(heart_treat_rect)	
- 	if overlap_heart.height >= 15 and overlap_heart.width >= 15:
- 		heart_treat_drop_speed = random.randrange(7, 11, 1)
- 		heart_treat_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
- 																			(window_x - game_x) // 2 + game_x - dog_size // 2, heart_size), 
- 																			  window_y - game_border_y + 10)
- 		if hit >=1:
-	 		hit -= 1
-	 	elif hit == 0:
-	 		score += 30
-	 		opacity = 255
-	 		point_font_size = 30
-	 		point_rect.midbottom = dog_rect.midtop
- 			point3_float_time = time.time()
-	 	heart_treat = False
+  overlap_heart = dog_rect.clip(heart_treat_rect)
+  if overlap_heart.height >= 15 and overlap_heart.width >= 15:
+    heart_treat_drop_speed = random.randrange(7, 11, 1)
+    heart_treat_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2,
+                                                   (window_x - game_x) // 2 + game_x - dog_size // 2, heart_size), 
+                                  window_y - game_border_y + 10)
+    if hit >=1:
+      hit -= 1
+    elif hit == 0:
+      score += 30
+      opacity = 255
+      point_font_size = 30
+      point_rect.midbottom = dog_rect.midtop
+      point3_float_time = time.time()
+    heart_treat = False
 	 	
-	 # floating point after catched heart				
- 	try:
- 		if time.time() - point3_float_time < 0.7:	
- 			opacity, point_font_size, point_rect = get_point(mud, 'Roboto', point_font_size, opacity, point_rect, 30)
- 	except: pass
- 	
- 	# treat 1 drop
- 	hit_bottom1, treat1_rect = treat_drop(treat, treat1_rect, game_rect.top, game_rect.bottom, treat1_drop_speed, hit_bottom1)
- 	
- 	if hit_bottom1:
- 		hit += 1
- 		treat1_drop_speed = random.randrange(7, 11, 1)
- 		treat1_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
-																			 (window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), 
-																	 		  window_y - game_border_y +10)
- 	
- 	
-	 	
- 	# getting points for treat 1
- 	overlap1 = dog_rect.clip(treat1_rect)	
- 	if overlap1.height >= 15 and overlap1.width >= 15:
- 		treat1_drop_speed = random.randrange(7, 11, 1)
- 		treat1_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
- 																			(window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), 
- 																			  window_y - game_border_y +10)
- 		score += 10
- 		# set up position for point to appear
- 		opacity = 255
- 		point_font_size = 30
- 		point_rect.midbottom = dog_rect.midtop
- 		point1_float_time = time.time()
+	 # floating point after catched heart
+  try:
+    if time.time() - point3_float_time < 0.7:	
+      opacity, point_font_size, point_rect = get_point(mud, 'Roboto', point_font_size, opacity, point_rect, 30)
+  except: pass
+
+  # treat 1 drop
+  hit_bottom1, treat1_rect = treat_drop(treat, treat1_rect, game_rect.top, game_rect.bottom, treat1_drop_speed, hit_bottom1)
+
+  if hit_bottom1:
+    hit += 1
+    treat1_drop_speed = random.randrange(7, 11, 1)
+    treat1_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
+                                              (window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), 
+                             window_y - game_border_y +10)
+  
+  # getting points for treat 1
+  overlap1 = dog_rect.clip(treat1_rect)
+  if overlap1.height >= 15 and overlap1.width >= 15:
+    treat1_drop_speed = random.randrange(7, 11, 1)
+    treat1_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
+                                              (window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]),
+                             window_y - game_border_y +10)
+    score += 10
+    # set up position for point to appear
+    opacity = 255
+    point_font_size = 30
+    point_rect.midbottom = dog_rect.midtop
+    point1_float_time = time.time()
  		
  	# start delay time at a certain point 
- 	if score >= 50 and not spawn_second:
- 		spawn_time = time.time()
- 		spawn_second = True
+  if score >= 50 and not spawn_second:
+    spawn_time = time.time()
+    spawn_second = True
  	
  	# floating point after catched treat
- 	try:				
- 		if time.time() - point1_float_time < 0.7:	
- 			opacity, point_font_size, point_rect = get_point(mud, 'Roboto', point_font_size, opacity, point_rect, 10)
- 	except: pass
- 	
- 	# spawn second treat after a delay
- 	if spawn_second and time.time() - spawn_time >= 3.0:
- 		hit_bottom2, treat2_rect = treat_drop(treat, treat2_rect, game_rect.top, game_rect.bottom, treat2_drop_speed, hit_bottom2)
- 	if hit_bottom2:
- 		hit += 1
- 		treat2_drop_speed = random.randrange(7, 11, 1)
- 		treat2_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
-																			 (window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), 
-																	 		  window_y - game_border_y +10)
- 	
- 	# getting points for treat 2
- 	overlap2 = dog_rect.clip(treat2_rect)
- 	if overlap2.height >= 15 and overlap2.width >= 15:
- 		treat2_drop_speed = random.randrange(7, 11, 1)
- 		treat2_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
- 																			(window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), 
- 																			  window_y - game_border_y +10)
- 		score += 10
- 		# set up position for point to appear
- 		opacity = 255
- 		point_font_size = 30
- 		point_rect.midbottom = dog_rect.midtop
- 		point2_float_time = time.time()
- 	
- 	# floating point after catched treat 2
- 	try:				
- 		if time.time() - point2_float_time < 0.7:	
- 			opacity, point_font_size, point_rect = get_point(mud, 'Roboto', point_font_size, opacity, point_rect, 10)
- 	except: pass
+  try:				
+    if time.time() - point1_float_time < 0.7:	
+      opacity, point_font_size, point_rect = get_point(mud, 'Roboto', point_font_size, opacity, point_rect, 10)
+  except: pass
+
+  # spawn second treat after a delay
+  if spawn_second and time.time() - spawn_time >= 3.0:
+    hit_bottom2, treat2_rect = treat_drop(treat, treat2_rect, game_rect.top, game_rect.bottom, treat2_drop_speed, hit_bottom2)
+  if hit_bottom2:
+    hit += 1
+    treat2_drop_speed = random.randrange(7, 11, 1)
+    treat2_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
+                                              (window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), 
+                             window_y - game_border_y +10)
+
+  # getting points for treat 2
+  overlap2 = dog_rect.clip(treat2_rect)
+  if overlap2.height >= 15 and overlap2.width >= 15:
+    treat2_drop_speed = random.randrange(7, 11, 1)
+    treat2_rect.midbottom = (random.randrange((window_x - game_x) // 2 + dog_size // 2, 
+                                              (window_x - game_x) // 2 + game_x - dog_size // 2, treat_size[0]), 
+                             window_y - game_border_y +10)
+    score += 10
+    # set up position for point to appear
+    opacity = 255
+    point_font_size = 30
+    point_rect.midbottom = dog_rect.midtop
+    point2_float_time = time.time()
+
+  # floating point after catched treat 2
+  try:				
+    if time.time() - point2_float_time < 0.7:	
+      opacity, point_font_size, point_rect = get_point(mud, 'Roboto', point_font_size, opacity, point_rect, 10)
+  except: pass
  	
  	# display health
- 	health_check(hearts, hit)	
+  health_check(hearts, hit)	
  	# game ends if take more than 3 hits
- 	if hit >= 3:
- 		game_over(game_rect)
- 		game_reset()
- 						
- 	pygame.display.update()
- 	fps.tick(game_speed)
+  if hit >= 3:
+    game_over(game_rect)
+    game_reset()
+  
+  pygame.display.update()
+  fps.tick(game_speed)
 
 pygame.quit()
